18c454b6972cc9a3b261af1eeeaa1de4828cbc50
lilalajemima <l.runiga@alustudent.com>
2024-06-26 13:55:50 -0700

    check

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# What ord does is deternime the index number of letters
+def islower(letter):
+    if ord(letter) > 96 and ord(letter) < 123:
+        return True
+    else:
+        return False

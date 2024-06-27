3b6a7aa92714f867d7bc631612691a4e06116ad7
lilalajemima <l.runiga@alustudent.com>
2024-06-27 16:21:42 -0700

    pep8 style errors fixed

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def divisible_by_2(my_list=[]):
     new_list = []
     for i in range(len(my_list)):
--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def no_c(my_string):
     new_string = ""
     for ch in my_string:

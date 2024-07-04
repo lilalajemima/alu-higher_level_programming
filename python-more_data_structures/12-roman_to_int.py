4b5e34557201184b070359e41a4025415df62646
lilalajemima <l.runiga@alustudent.com>
2024-07-04 05:16:45 -0700

    set up

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def roman_to_int(roman_string):
     if roman_string is None or \
             isinstance(roman_string, str) is False:
---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    num_of_keys = 0
+
+    for k in a_dictionary:
+        num_of_keys += 1
+
+    return num_of_keys

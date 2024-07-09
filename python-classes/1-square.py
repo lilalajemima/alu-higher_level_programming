92ba460f28ba519141f08e4c2d23120e6437c3ea
lilalajemima <l.runiga@alustudent.com>
2024-07-09 07:58:49 -0700

    trial

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-          """ 
-              define a class called 'Square'
-          """
+""" 
+   define a class called 'Square'
+"""
 
-class Square
-          """ 
-              square has a private instance attribute : 'size'
-          """
-          def __init__(self, size):
-              """
-                 Args:
-                      size (int): size of the square (object)
-              """
-              self.__size = size 
 
+class Square:
+    """
+    square has a private instance attribute : 'size'
+    """
+
+    def __init__(self, size):
+        """
+        Args:
+             size (int): size of the square (object)
+        """
+        self.__size = size

588575a882eaf304d6ed4815e220c662a7020d5a
lilalajemima <l.runiga@alustudent.com>
2024-07-09 07:37:44 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+          """ 
+              define a class called 'Square'
+          """
+
+class Square
+          """ 
+              square has a private instance attribute : 'size'
+          """
+          def __init__(self, size):
+              """
+                 Args:
+                      size (int): size of the square (object)
+              """
+              self.__size = size 
+

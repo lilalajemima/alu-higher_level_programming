86db11e23b0b13107390c8ba82083e8d9f714f92
lilalajemima <l.runiga@alustudent.com>
2024-07-10 08:03:43 -0700

    task 2 and task 3

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    define a class 'Square'
+"""
+
+
+class Square:
+    """
+        square with private instance attribute: 'size'
+    """
+
+    def __init__(self, size=0):
+        """
+            Args:
+                size (int): size of the new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

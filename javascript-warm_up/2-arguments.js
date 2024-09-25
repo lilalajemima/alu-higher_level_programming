4086c9260527d01fee737b9e892555efefc3e2aa
lilalajemima <l.runiga@alustudent.com>
2024-09-25 16:53:01 -0700

    added she bangs

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
+const myArgs = process.argv.slice(2);
+if (myArgs.length === 0) {
   console.log('No argument');
-} else if (args.length === 1) {
+} else if (myArgs.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
